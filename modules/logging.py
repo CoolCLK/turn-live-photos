3f127f04da6c3eefcc6282b36dfb4898f87aa37d
CoolCLK <cool_clk@outlook.com>
2025-06-02 10:36:16 +0800

    更新 debug

--- modules/logging.py
+++ modules/logging.py
@@ -21,11 +21,11 @@ def get_logger(name):
     :rtype: logging.Logger
     """
     logging.basicConfig(
-        level = logging.INFO,
+        level = logging.DEBUG,
         format = colorama.Fore.LIGHTBLACK_EX + '[' + colorama.Fore.CYAN + 'turn-live-photos' + colorama.Fore.LIGHTBLACK_EX + '/' + colorama.Fore.LIGHTBLUE_EX + '%(name)s' + colorama.Fore.LIGHTBLACK_EX + '] [' + colorama.Fore.GREEN + '%(asctime)s' + colorama.Fore.LIGHTBLACK_EX + '] [' + colorama.Fore.RED + '%(levelname)s' + colorama.Fore.LIGHTBLACK_EX + '] ' + colorama.Fore.RESET + '%(message)s',
         datefmt = '%Y-%m-%d %H:%M:%S'
     )
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
     
     return logger
